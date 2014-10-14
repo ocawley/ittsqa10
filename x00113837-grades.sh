65c719050407b394a13ad2b0d0281eab14bc7436
x00113837 <x00113837@gmail.com>
2014-10-14 17:12:49 +0100

    found a bug output 50 50 should be a pass but its a fail

---
+++ x00113837-grades.sh
@@ -0,0 +1,42 @@
+# test case1
+#inputs: 40 40 
+#expected outputs : fail
+#actual output:fail
+
+java grades 40 40 
+
+
+#test case2
+#inputs :50 50
+#expected output: pass 
+#actual output:fail
+java grades 50 50 
+
+
+
+
+#test case3
+#inputs :50 51
+#expected output: pass 
+#actual output:fail
+java grades 50 51
+
+
+#test case4
+#inputs :55 55
+#expected output: pass 
+#actual output:fail
+java grades 55 55
+
+
+#test case5
+#inputs :59 59
+#expected output: pass 
+#actual output:fail
+java grades 59 59
+
+#test case6
+#inputs :66 66
+#expected output: pass 
+#actual output:pass
+java grades 66 66
